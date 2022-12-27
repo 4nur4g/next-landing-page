2f30f614ba9cb8cbc9e8c6b9ea138c9c0655a551
Anurag Pradhan <33851499+4nur4g@users.noreply.github.com>
2022-12-27 19:03:18 +0530

    Commit in another branch

--- pages/index.js
+++ pages/index.js
@@ -17,7 +17,7 @@ export default function Home() {
       <main className={styles.main}>
         <div className={styles.description}>
           <p>
-            👀 Det started by editing&nbsp;
+            🌸 Another branch Det started by editing&nbsp;
             <code className={styles.code}>pages/index.js</code>
           </p>
           <div>
